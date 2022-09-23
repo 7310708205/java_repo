282d88bb9c4821baa1d7b774be1b313196acbf58
babita15 <68740109+7310708205@users.noreply.github.com>
2022-09-23 18:12:28 +0530

    Not required

--- src/EngineeringStudent.java
+++
@@ -1,7 +0,0 @@
-public class EngineeringStudent extends StudentDetails{
-    public static void main(String[] args) {
-        EngineeringStudent es = new EngineeringStudent();
-        System.out.println(es.name);
-        System.out.println(es.age);
-    }
-}
